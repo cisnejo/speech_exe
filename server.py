673fb4c632f3e93c7231fbf73ca331522255f50a
cisnejo <cisnerosjoseph122@gmail.com>
2022-12-23 13:04:11 -0800

    able to create db and add record

--- server.py
+++ server.py
@@ -1,21 +1,49 @@
 
 import json
 from flask import Flask
-
+from speech import *
 
 import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy.sql import func
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
 def hello():
-    value = {
-        "test": "works"
-    }
-    return json.dumps(value)
+    class PathCommands(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        command = db.Column(db.String(100), nullable=False)
+        path = db.Column(db.String(100), nullable=False)
+        created_at = db.Column(db.DateTime(timezone=True),
+                               server_default=func.now())
 
+        def __repr__(self):
+            return f'<PathCommands {self.firstname}>'
+
+    command = 'League of Legends'
+    path = '/path/to/Lol'
+
+    path_command = PathCommands(
+        command=command,
+        path=path
+    )
+    db.session.add(path_command)
+    db.session.commit()
+
+    speech_text = speak()
+    return json.dumps({"speech_text": speech_text})
 
 
 if __name__ == "__main__":
--- speech.py
+++ speech.py
@@ -1,2 +1,20 @@
 import speech_recognition as sr
-import pyttsx
+import pyttsx3
+from playsound import playsound
+
+def speak():
+
+    r = sr.Recognizer()
+
+    def SpeakText(command):
+        engine = pyttsx3.init()
+        engine.say(command)
+        engine.runAndWait()
+
+    with sr.Microphone() as source2:
+        r.adjust_for_ambient_noise(source2, duration=0.2)
+        audio2 = r.listen(source2)
+        MyText = r.recognize_google(audio2)
+        MyText = MyText.lower()
+        playsound('./sounds/lil-jon-okay.mp3')
+        return MyText
